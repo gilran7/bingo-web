4150896d730371aa3ca7f4c6c5bd48d56569dc9f
gilran7 <tu_email_de_github@ejemplo.com>
2025-08-15 13:31:55 -0400

    restaurar aqui

--- netlify/functions/save-cards.js
+++ netlify/functions/save-cards.js
@@ -1,17 +1,5 @@
-// /netlify/functions/save-cards.js
-export async function handler(event, context) {
-    try {
-        const { cards } = JSON.parse(event.body);
-        // Aquí guardas los cartones, ejemplo: en un archivo JSON o base de datos
-        console.log("Cartones recibidos:", cards);
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Cartones guardados con éxito" })
-        };
-    } catch (err) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: err.message })
-        };
-    }
-}
+const store = getStore({
+    name: "bingo-cards-venta",
+    siteID: process.env.NETLIFY_SITE_ID,
+    token: process.env.NETLIFY_API_TOKEN,
+});
