ba58e99a375d82080a8dd9b08e093b80a65a7792
gilran7 <tu_email_de_github@ejemplo.com>
2025-08-15 09:20:41 -0400

    Savecar actualizada

--- netlify/functions/save-cards.js
+++ netlify/functions/save-cards.js
@@ -4,11 +4,18 @@ import { jsonResponse } from "./_utils.js";
 export async function handler(event) {
   try {
     const { cards } = JSON.parse(event.body);
-    const store = getStore("bingo-cards");
+
+    const store = getStore({
+      name: "bingo-cards",
+      siteID: process.env.NETLIFY_SITE_ID,
+      token: process.env.NETLIFY_API_TOKEN
+    });
+
     await store.set("cards", JSON.stringify(cards));
+
     return jsonResponse({ message: "Cartones guardados con éxito" });
   } catch (error) {
-    console.error("Error en save-cards.js:", error); // Para verlo en el log de Netlify
+    console.error("Error en save-cards.js:", error);
     return jsonResponse({ error: error.message || error.toString() }, 500);
   }
 }
